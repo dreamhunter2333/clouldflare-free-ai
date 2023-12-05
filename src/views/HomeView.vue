0101c8808d180f40104d0d790f3e332453c0dab7
dreamhunter2333 <dreamhunter2333@gmail.com>
2023-12-05 14:50:28 +0800

    feat: chat ui

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,30 +1,41 @@
 <script setup>
-import { NFormItem, NInputGroup } from 'naive-ui';
-import { NInput, NCard, NButton, useMessage } from 'naive-ui';
+import { NFormItem, NInputGroup, NScrollbar, NAvatar } from 'naive-ui';
+import { NInput, NCard, NButton, useMessage, NIcon } from 'naive-ui';
 import { ref } from 'vue';
-import MarkdownIt from 'markdown-it';
+import { SmartToyOutlined, ManOutlined } from '@vicons/material';
 
 import { api } from '../api';
 
 const message = useMessage();
-const md = new MarkdownIt();
 
 const systemPrompt = ref('');
 const userPrompt = ref('');
-const result = ref('');
+const messageHistory = ref([]);
 
-const generate = async () => {
+const send = async () => {
   if (!userPrompt.value) {
     message.error('请输入提示词');
     return;
   }
+  messageHistory.value.push({
+    role: 'user',
+    content: userPrompt.value
+  });
   try {
+    const inputs = []
+    if (systemPrompt.value) {
+      inputs.push({ role: 'system', content: systemPrompt.value })
+    }
+    inputs.push(...messageHistory.value.slice(-4))
     const res = await api.textGeneration([
       { role: 'system', content: systemPrompt.value },
       { role: 'user', content: userPrompt.value }
     ]);
     const { response } = res;
-    result.value = md.render(response);
+    messageHistory.value.push({
+      role: 'assistant',
+      content: response
+    });
   } catch (e) {
     message.error(e.message);
   }
@@ -34,21 +45,33 @@ const generate = async () => {
 
 <template>
   <main>
-    <h2>文本生成</h2>
+    <h2>AI 聊天</h2>
     <n-form-item label="系统提示词" label-placement="left">
-      <n-input class="left" v-model:value="systemPrompt" type="textarea" :autosize="{
-        minRows: 2
-      }" />
+      <n-input class="left" v-model:value="systemPrompt" />
     </n-form-item>
-    <n-form-item label="用户提示词" label-placement="left">
-      <n-input class="left" v-model:value="userPrompt" type="textarea" :autosize="{
-        minRows: 2
-      }" />
-    </n-form-item>
-    <n-button @click="generate" type="primary">生成</n-button>
-    <n-card v-if="result">
-      <div class="left" v-html="result"></div>
+    <n-card>
+      <n-scrollbar style="height: 50vh;" trigger="none">
+        <div v-for="msg in messageHistory" v-bind:key="msg" :class="msg.role == 'user' ? 'right' : 'left'">
+          <div v-if="msg.role != 'user'" class="chat-item">
+            <n-avatar>
+              <n-icon :component="SmartToyOutlined" />
+            </n-avatar>
+          </div>
+          <div class="chat-item">
+            <n-card size="small">{{ msg.content }}</n-card>
+          </div>
+          <div v-if="msg.role == 'user'" class="chat-item">
+            <n-avatar>
+              <n-icon :component="ManOutlined" />
+            </n-avatar>
+          </div>
+        </div>
+      </n-scrollbar>
     </n-card>
+    <n-input-group>
+      <n-input class="left" v-model:value="userPrompt" />
+      <n-button @click="send" type="primary">发送</n-button>
+    </n-input-group>
   </main>
 </template>
 
@@ -57,11 +80,21 @@ const generate = async () => {
   margin-top: 10px;
 }
 
-.n-card {
-  margin-top: 10px;
-}
-
 .left {
   text-align: left;
+}
+
+.n-scrollbar {
+  display: flex;
+  flex-direction: column-reverse;
+}
+
+.right {
+  text-align: right;
+}
+
+.chat-item {
+  display: inline-block;
+  vertical-align: middle;
 }
 </style>
